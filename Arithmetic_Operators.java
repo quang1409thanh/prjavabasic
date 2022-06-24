cfa50c96f5d18bf94e91ebeff865fff400c46610
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-06-25 03:30:08 +0700

    commit lec6 Arithmetic operators

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,23 @@
+
+public class Arithmetic_Operators {
+    
+    public static void main(String[] args) {
+
+        double firstNumber=2.5;
+        double secondNumber=3.5;
+        double result;
+        
+        result=firstNumber+secondNumber;
+        System.out.println("results of addition is: " + result);
+
+        result=firstNumber-secondNumber;
+        System.out.println("result of subtraction is: "+ result);
+
+        result=firstNumber/secondNumber;
+        System.out.println("result of devision is: "+ result);
+
+        result=firstNumber*secondNumber;
+        System.out.println("result of multiplication is: "+ result);
+    
+    }
+}
