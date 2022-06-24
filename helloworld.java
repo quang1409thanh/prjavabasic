7b6ba9f7034a3e018eb204942a4603ac39b72d0f
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-06-25 01:40:19 +0700

    commit lec4 variables

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        double b=3.5;
+        float  c=2.645f;
+        System.out.println(b);
+        System.out.println(c);
+        String s="Please write some text:";
+        System.out.println(s);
+    }
+}g
